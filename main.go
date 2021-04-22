7beeb125b77a0f5b6ef5f472a1ecd9952830ac74
mosn1bw <81889769+mosn1bw@users.noreply.github.com>
2021-04-22 17:41:27 +0430

    Update main.go

--- main.go
+++ main.go
@@ -111,7 +111,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 						
 		if strings.EqualFold(message.Text, "36") {
 			switch message := event.Message.(type) {
-		        	if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(" Bye bye!")).Do(); err != nil {
+		        	if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(" Bye bye!")).Do(); err != nil },
 				log.Print(err)
 			}
 			
